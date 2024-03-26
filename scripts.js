9f9bcb03561e5ee2f28184687a543a0a6a5a6e1c
Enzo Mescall <emm121@duke.edu>
2024-03-26 14:03:17 -0400

    testing forwards/backwards buttons

--- scripts.js
+++ scripts.js
@@ -2,13 +2,14 @@ window.onload = function() {
     fetchLetters();
 };
 
-function fetchLetters() { 
+function fetchLetters() {
     fetch('letters.json') // Assume you have a JSON file with details of letters
         .then(response => response.json())
         .then(data => {
             const letterList = document.getElementById('letterList');
             data.forEach(letter => {
                 const listItem = document.createElement('li');
+                listItem.classList.add('letter-item'); // Add class for styling
                 const link = document.createElement('a');
                 link.href = `letter.html?id=${letter.id}`; // Link to individual letter page
                 link.textContent = `${letter.date} - ${letter.title}`;
