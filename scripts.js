65b5574b65f43d63e217efd659530e18aaf4b219
Enzo Mescall <emm121@duke.edu>
2024-03-25 22:26:51 -0400

    testing this out

---
+++ scripts.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    fetchLetters();
+};
+
+function fetchLetters() { 
+    fetch('letters.json') // Assume you have a JSON file with details of letters
+        .then(response => response.json())
+        .then(data => {
+            const letterList = document.getElementById('letterList');
+            data.forEach(letter => {
+                const listItem = document.createElement('li');
+                const link = document.createElement('a');
+                link.href = `letter.html?id=${letter.id}`; // Link to individual letter page
+                link.textContent = `${letter.date} - ${letter.title}`;
+                listItem.appendChild(link);
+                letterList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error fetching letters:', error));
+}
